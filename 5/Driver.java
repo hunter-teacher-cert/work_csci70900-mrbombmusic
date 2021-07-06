97e8174b43bddf7087dc0f3f529bc7ac9abb21c6
Liam Baum <liamcbaum@gmail.com>
2021-07-06 12:48:32 -0400

    added Time and Driver

---
+++ 5/Driver.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+/*
+- Your driver file should contain code to (in suggested development order)
+instantiate a newly-created Time object (aka "instance of class Time") with default value(s)
+instantiate a newly-created Time object with specified value(s)
+assign a Time var the value null
+assign a Time var the value of an existing Time var
+print all of the above (See multiple ways to do this? Try all -- and document in comments!)
+test for equality of each of the Time instances above. Print results.
+add two Time objects and print results
+add two Time objects and save the result in another Time object
+
+
+- Your Time class definition file should contain code to
+tell a Time object how to do the tasks necessary to achieve the above
+
+*/
+
+public class Driver {
+
+  public static void main(String[] args) {
+
+    //declare a var of appropriate type to assign an instance of Time to
+    Time t;
+
+    //assign var the address of a newly-apportioned Time object
+    t = new Time();
+  }//end main()
+
+}//end class
