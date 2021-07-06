39fff7815bcc1959cfbb1f76c266992ac57847ca
Liam Baum <liamcbaum@gmail.com>
2021-07-06 10:16:26 -0400

    cleaned up code and added readme

--- 5/Fib.java
+++ 5/Fib.java
@@ -1,44 +1,19 @@
+/*
+Programing in a High Level Language
+In Class Work - 07/06/21
+Fibinacci Recursion lab
+by
+Liam Baum
+Mamudu Wally
+Eduardo Leite
+
+*/
+
 import java.io.*;
 import java.util.*;
 
 
 class Fib{
-
-  /*
-  0, 1, 1, 2, 3, 5, 8, 13, 21 (n = 8), 34, 55, 89
-
- n = 8
-
- (n - (n -1)) + (n - (n - 2))
- fib(n)
-
- sum = 8 - 7 = 1
-
- sum += n - (n-1)
- 2 = 1 + 1
- return sum;
-
-
-  input is nth time of the sequence
-
-  base case = (n- 2) + (n -1)
-
-  int totalSum = 0;
-
-  if n == 0 {
-  return 0;
-} else if (n == 1) {
-return 1;
-} else {
-totalSum = 1+0;
-}
-
- sum = n(9) n(9 - 1)
-
-n = position;
-
-return int - sum of input
-*/
 
 // this works!!!
 public static int fib(int n) {
@@ -52,18 +27,6 @@ public static int fib(int n) {
   }
   return totalSum;
 }
-
-// public static int fib2(int n) {
-//   int totalSum = 0;
-//   if(n - 1 == 0) {
-//     return 0;
-//   } else {
-//     totalSum = fib2(n - 2) + fib2(n - 1);
-//   }
-//   return totalSum;
-// }
-
-
 
 public static void main(String[] args) {
   System.out.println("Return value = " + fib2(2));
---
+++ 5/README.md
@@ -0,0 +1,34 @@
+
+# Programing in a High Level Language
+# In Class Work - 07/06/21
+# Fibinacci Recursion lab
+## by
+## Liam Baum
+## Mamudu Wally
+## Eduardo Leite
+
+
+**Notes**
+
+0, 1, 1, 2, 3, 5, 8, 13, 21 (n = 8), 34, 55, 89 <br>
+
+
+input is nth time of the sequence  <br>
+
+base case = (n- 2) + (n -1)  <br>
+
+int totalSum = 0;  <br>
+
+if n == 0 {  <br>
+return 0;  <br>
+} else if (n == 1) {  <br>
+return 1;  <br>
+} else {  <br>
+totalSum = 1+0;  <br>
+}  <br>
+
+sum = n(9) n(9 - 1)  <br>
+
+n = position;  <br>
+
+return int - sum of input  <br>
