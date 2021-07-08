d37f9f11482520ea4e1571de3a15071aa4af7597
Liam Baum <liamcbaum@gmail.com>
2021-07-07 22:16:19 -0400

    rewrote code alone to make sure I understood. Included shrink method. array never includes non-menaingful elements

---
+++ 6/superarray/Driver.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Driver{
+
+  public static void main(String[] args) {
+
+    // make new object with default instance variables
+    SuperArray sa = new SuperArray();
+
+    // make new object with parameterized constructor
+    SuperArray saSpec = new SuperArray(3);
+
+    // print initialized objects - toString method automatically invoked
+    System.out.println(sa); // array with 2 values - [0, 0]
+    System.out.println(saSpec); // array with 3 values - [0, 0, 0]
+
+    // checks isEmpty method - both should evaluate as true
+    System.out.println(sa.isEmpty());
+    System.out.println(saSpec.isEmpty());
+
+    // checks add method one at a time
+    // includes 3 add methods to test private grow method in class
+    sa.add(8);
+    sa.add(4);
+    sa.add(6);
+    System.out.println(sa); // expect [8, 4, 6] - confirmed
+
+    // checks add method in loop
+    // includes 5 values to to test private grow method in class
+    for(int i = 1; i < 30; i++) {
+      saSpec.add(i);
+    }
+    System.out.println(saSpec);
+    System.out.println("");
+
+    // loop to check getLength & getValue getter methods and remove method in loop
+    for(int i = 0; i < saSpec.getLength(); i++){
+      if(saSpec.getValue(i) % 3 == 0) { // if element in array is divisible by 3...
+        saSpec.remove(i); // ... remove value divisible by 3
+      }
+    }
+
+    System.out.println(saSpec); // [1, 2, 3, 4, 5]
+    System.out.println("");
+
+    sa.insert(3, 1); // inserts 3 into index 1 of array
+    System.out.println(sa); // [8, 3, 4, 6]
+    System.out.println("");
+    sa.add(2); // adds 2 to the end of array
+    System.out.println(sa); // [8, 3, 4, 6, 2]
+    System.out.println("");
+    sa.insert(10, 3); // inserts 10 into index 3 of array
+    System.out.println(sa); // [8, 3, 4, 10, 6, 2]
+    System.out.println("");
+    sa.remove(2); // removes element from index 2
+    sa.remove(1); // removes element from index 1
+    sa.remove(0); // / removes element from index 0
+    System.out.println(sa); // [10, 6, 2]
+
+
+  } // end main
+} // end class
---
+++ 6/superarray/SuperArray.java
@@ -0,0 +1,111 @@
+import java.io.*;
+import java.util.*;
+
+
+public class SuperArray{
+
+  // declare instance variables
+  private int numberElements; // number of meaningful elements in array - also index after last meaningful element
+  private int[] data; // array that holds values
+
+  // constructor to intitialize instance variables - array gets 2 values
+  public SuperArray() {
+    this.numberElements = 0;
+    this.data = new int[2];
+  }
+
+  // parameterized constructor where size of array is provided as argument
+  public SuperArray(int size) {
+    this.numberElements = 0;
+    this.data = new int[size];
+  }
+
+  // method to check if array does not contain any meaningful elements
+  public boolean isEmpty() {
+    return this.numberElements == 0;
+  }
+
+  // method to add meaningful element to end of array
+  public void add(int value) {
+    // check to see that array is large enough to take one more value
+    // uses numberElements as reference for
+    if(this.numberElements < this.data.length) {
+      this.data[this.numberElements] = value; // adds value argument to end of array
+      this.numberElements++; // increments number of meaningful values in array
+    } else { // if array is not big enough to add one more value...
+      grow(); //...call grow method to increase size of array
+      this.data[this.numberElements] = value; // add value argument to end of array
+      this.numberElements++; // increments number of meaningful values in array
+    }
+  }
+
+  // method to add value to array at specific index
+  public void insert(int value, int index) {
+  // check to see that array is large enough to take one more value
+  if(this.numberElements == this.data.length) { // if array has no more space to hold new value...
+    grow(); // ...increase size of array
+  }
+  // loop through array starting at the end moving down to the index where new value will be added
+  for(int i = this.numberElements; i > index; i--) {
+    this.data[i] = this.data[i - 1]; // shift each value up by one index
+  }
+  this.data[index]= value; // insert value argument at specified index
+  this.numberElements++; // // increments number of meaningful elements in array
+  }
+
+  // method to remove a value from array at specific index specified by argument
+  public void remove(int index) {
+    // loops trhough array starting at chosen index to end of array
+    for(int i = index; i < this.numberElements -1; i++) {
+      this.data[i] = this.data[i + 1]; // shift each value down by one index
+    }
+    this.data[this.numberElements -1] = 0; // changes last value of array to 0 (non-meaningful element)
+    this.numberElements--; // decrements number of meaningful elements in array
+    shrink(); // resizes array by removing non-meaningful elements from end of array
+  }
+
+  private void grow(){
+    // initializes new array that has one additional index space
+    int[] newData = new int[this.numberElements + 1];
+    // copies all values from data array to new array
+    for(int i = 0; i < this.data.length; i++) {
+      newData[i] = this.data[i];
+    }
+    // points data array to new array
+    this.data = newData;
+  }
+
+  private void shrink() {
+    // initializes new array the size of number of meaningful elements
+    // this will be one less than the current size of array since numberElements is decremented prior to invoking shrink()
+    int[] newData = new int[this.numberElements];
+    // copies all meaningful elements from data array to new array
+    for(int i = 0; i < this.numberElements; i++) {
+      newData[i] = this.data[i];
+    }
+    // points data array to new array
+    this.data = newData;
+  }
+
+  // returns number of meaningful elements in array
+  public int getNumberElements(){
+    return this.numberElements;
+  }
+
+  // returns length of array
+  public int getLength() {
+    return this.data.length;
+  }
+
+  // returns value in array at speicifc index provided as argument
+  public int getValue(int index) {
+    return this.data[index];
+  }
+
+  // overriding toString method to return full array as string
+  public String toString() {
+    return String.format(Arrays.toString(this.data));
+  }
+
+
+}
