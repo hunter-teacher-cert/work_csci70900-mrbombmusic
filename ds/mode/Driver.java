e1f1a1ed95d424dacd66ecf23c954996a760ea46
Liam Baum <liamcbaum@gmail.com>
2021-07-22 22:57:17 -0400

    added mode files

---
+++ ds/mode/Driver.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Driver{
+
+    public static void main(String[] args) {
+	// Mode m = new Mode();
+
+	// System.out.println(m);
+  // int smallestValue = m.findSmallestValue();
+  // System.out.println(smallestValue);
+  // int v = m.frequency(6);
+  // System.out.println( "6 appears in your data set " + v + " times.");
+  // int numMode = m.calcMode();
+  // System.out.println("The mode of this data set is " + numMode);
+
+
+  long start;
+  long elapsed;
+	int size;
+
+	// size = 200000;
+	// SortDemo sds = new SortDemo(size);
+	// start = System.currentTimeMillis();
+	// sds.sort();
+	// elapsed = System.currentTimeMillis() - start;
+	// System.out.println("Selection on size " + size + " : " + elapsed);
+
+	Mode m = new Mode(1000);
+	start = System.currentTimeMillis();
+	int m1 = m.calcMode1();
+	elapsed = System.currentTimeMillis() - start;
+	System.out.println("The mode of this data set is " + m1 + " : " + elapsed);
+
+	Mode mCheck = new Mode(1000);
+	start = System.currentTimeMillis();
+	int m2 = mCheck.calcMode2();
+	elapsed = System.currentTimeMillis() - start;
+	System.out.println("The mode of this data set is " + m2 + " : " + elapsed);
+
+    }
+
+}
